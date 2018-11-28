f7eaf639bda95deb108ca5c22920b67841c42395
Tadahiro Kato <goemon12.tc4@gmail.com>
2018-11-29 06:42:21 +0900

    Initial Commit

---
+++ TOKEI-R4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TOKEI-R4
+//
+//  Created by goemon12 on 2018/11/29.
+//  Copyright © 2018 goemon12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
