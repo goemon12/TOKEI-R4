40e490de45ca4eaf1ae9765fb4fe84ee53aac2ff
Tadahiro Kato <goemon12.tc4@gmail.com>
2019-01-13 21:35:05 +0900

    文字位置と移動を変数にして改造しやすいように変更。

--- TOKEI-R4/ViewController.swift
+++ TOKEI-R4/ViewController.swift
@@ -48,58 +48,74 @@ class ViewController: UIViewController {
     }
 
     func stupNode() {
+        //文字位置と移動を変数にしました。
+        //↓変更はここから
+        let pos_sx: CGFloat =  10 //秒 左下座標X
+        let pos_sy: CGFloat =  90 //秒 左下座標Y
+        let mov_sw: CGFloat = 300 //秒 横幅X
+        let mov_sh: CGFloat = 300 //秒 縦幅Y
+        let pos_mx: CGFloat =  40 //分 左下座標X
+        let pos_my: CGFloat = 120 //分 左下座標Y
+        let mov_mw: CGFloat = 240 //分 横幅X
+        let mov_mh: CGFloat = 240 //分 縦幅Y
+        let pos_hx: CGFloat =  70 //時 左下座標X
+        let pos_hy: CGFloat = 150 //時 左下座標Y
+        let mov_hw: CGFloat = 180 //時 横幅X
+        let mov_hh: CGFloat = 180 //時 縦幅Y
+        //↑変更はここまで
+
         for i in 0 ..< 60 {
             nodeS[i].text = "\(i)"
             nodeS[i].fontSize = 12
             nodeS[i].horizontalAlignmentMode = .center
             nodeS[i].verticalAlignmentMode = .center
             if i < 15 {
-                nodeS[i].position.x =  10
-                nodeS[i].position.y =  90 + 300 / 15 * CGFloat(i -  0)
+                nodeS[i].position.x = pos_sx
+                nodeS[i].position.y = pos_sy + mov_sh / 15 * CGFloat(i -  0)
                 nodeS[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeS[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeS[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: 0, y:  300 / 15, duration: 0),
-                    SKAction.moveBy(x: 0, y: -300 / 15, duration: 1)])))
+                    SKAction.moveBy(x: 0, y:  mov_sh / 15, duration: 0),
+                    SKAction.moveBy(x: 0, y: -mov_sh / 15, duration: 1)])))
             }
             else if i < 30 {
-                nodeS[i].position.x =  10 + 300 / 15 * CGFloat(i - 15)
-                nodeS[i].position.y = 390
+                nodeS[i].position.x = pos_sx + mov_sw / 15 * CGFloat(i - 15)
+                nodeS[i].position.y = pos_sy + mov_sh
                 nodeS[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeS[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeS[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x:  300 / 15, y: 0, duration: 0),
-                    SKAction.moveBy(x: -300 / 15, y: 0, duration: 1)])))
+                    SKAction.moveBy(x:  mov_sw / 15, y: 0, duration: 0),
+                    SKAction.moveBy(x: -mov_sw / 15, y: 0, duration: 1)])))
             }
             else if i < 45 {
-                nodeS[i].position.x = 310
-                nodeS[i].position.y = 390 - 300 / 15 * CGFloat(i - 30)
+                nodeS[i].position.x = pos_sx + mov_sw
+                nodeS[i].position.y = pos_sy + mov_sh - mov_sh / 15 * CGFloat(i - 30)
                 nodeS[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeS[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeS[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: 0, y: -300 / 15, duration: 0),
-                    SKAction.moveBy(x: 0, y:  300 / 15, duration: 1)])))
+                    SKAction.moveBy(x: 0, y: -mov_sh / 15, duration: 0),
+                    SKAction.moveBy(x: 0, y:  mov_sh / 15, duration: 1)])))
             }
             else {
-                nodeS[i].position.x = 310 - 300 / 15 * CGFloat(i - 45)
-                nodeS[i].position.y =  90
+                nodeS[i].position.x = pos_sx + mov_sw - mov_sw / 15 * CGFloat(i - 45)
+                nodeS[i].position.y = pos_sy
                 nodeS[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeS[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeS[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: -300 / 15, y: 0, duration: 0),
-                    SKAction.moveBy(x:  300 / 15, y: 0, duration: 1)])))
+                    SKAction.moveBy(x: -mov_sw / 15, y: 0, duration: 0),
+                    SKAction.moveBy(x:  mov_sw / 15, y: 0, duration: 1)])))
             }
             scene.addChild(nodeS[i])
             
@@ -108,52 +124,52 @@ class ViewController: UIViewController {
             nodeM[i].horizontalAlignmentMode = .center
             nodeM[i].verticalAlignmentMode = .center
             if i < 15 {
-                nodeM[i].position.x =  40
-                nodeM[i].position.y = 120 + 240 / 15 * CGFloat(i -  0)
+                nodeM[i].position.x = pos_mx
+                nodeM[i].position.y = pos_my + mov_mw / 15 * CGFloat(i -  0)
                 nodeM[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeM[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeM[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: 0, y:  240 / 15, duration: 0),
-                    SKAction.moveBy(x: 0, y: -240 / 15, duration: 60)])))
+                    SKAction.moveBy(x: 0, y:  mov_mh / 15, duration: 0),
+                    SKAction.moveBy(x: 0, y: -mov_mh / 15, duration: 60)])))
             }
             else if i < 30 {
-                nodeM[i].position.x =  40 + 240 / 15 * CGFloat(i - 15)
-                nodeM[i].position.y = 360
+                nodeM[i].position.x = pos_mx + mov_mw / 15 * CGFloat(i - 15)
+                nodeM[i].position.y = pos_my + mov_mh
                 nodeM[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeM[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeM[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x:  240 / 15, y: 0, duration: 0),
-                    SKAction.moveBy(x: -240 / 15, y: 0, duration: 60)])))
+                    SKAction.moveBy(x:  mov_mw / 15, y: 0, duration: 0),
+                    SKAction.moveBy(x: -mov_mh / 15, y: 0, duration: 60)])))
             }
             else if i < 45 {
-                nodeM[i].position.x = 280
-                nodeM[i].position.y = 360 - 240 / 15 * CGFloat(i - 30)
+                nodeM[i].position.x = pos_mx + mov_mw
+                nodeM[i].position.y = pos_my + mov_mh - mov_mh / 15 * CGFloat(i - 30)
                 nodeM[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeM[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeM[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: 0, y: -240 / 15, duration: 0),
-                    SKAction.moveBy(x: 0, y:  240 / 15, duration: 60)])))
+                    SKAction.moveBy(x: 0, y: -mov_mh / 15, duration: 0),
+                    SKAction.moveBy(x: 0, y:  mov_mh / 15, duration: 60)])))
             }
             else {
-                nodeM[i].position.x = 280 - 240 / 15 * CGFloat(i - 45)
-                nodeM[i].position.y = 120
+                nodeM[i].position.x = pos_mx + mov_mw - mov_mw / 15 * CGFloat(i - 45)
+                nodeM[i].position.y = pos_my
                 nodeM[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeM[i].text!)
                         let tmp2 = (tmp1! + 1) % 60
                         self.nodeM[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: -240 / 15, y: 0, duration: 0),
-                    SKAction.moveBy(x:  240 / 15, y: 0, duration: 60)])))
+                    SKAction.moveBy(x: -mov_mw / 15, y: 0, duration: 0),
+                    SKAction.moveBy(x:  mov_mw / 15, y: 0, duration: 60)])))
             }
             scene.addChild(nodeM[i])
         }
@@ -164,52 +180,52 @@ class ViewController: UIViewController {
             nodeH[i].horizontalAlignmentMode = .center
             nodeH[i].verticalAlignmentMode = .center
             if i < 6 {
-                nodeH[i].position.x =  70
-                nodeH[i].position.y = 150 + 180 / 6 * CGFloat(i -  0)
+                nodeH[i].position.x = pos_hx
+                nodeH[i].position.y = pos_hy + mov_hh / 6 * CGFloat(i -  0)
                 nodeH[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeH[i].text!)
                         let tmp2 = (tmp1! + 1) % 24
                         self.nodeH[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: 0, y:  180 / 6, duration: 0),
-                    SKAction.moveBy(x: 0, y: -180 / 6, duration: 3600)])))
+                    SKAction.moveBy(x: 0, y:  mov_hh / 6, duration: 0),
+                    SKAction.moveBy(x: 0, y: -mov_hh / 6, duration: 3600)])))
             }
             else if i < 12 {
-                nodeH[i].position.x =  70 + 180 / 6 * CGFloat(i -  6)
-                nodeH[i].position.y = 330
+                nodeH[i].position.x = pos_hx + mov_hw / 6 * CGFloat(i -  6)
+                nodeH[i].position.y = pos_hy + mov_hh
                 nodeH[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeH[i].text!)
                         let tmp2 = (tmp1! + 1) % 24
                         self.nodeH[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x:  180 / 6, y: 0, duration: 0),
-                    SKAction.moveBy(x: -180 / 6, y: 0, duration: 3600)])))
+                    SKAction.moveBy(x:  mov_hw / 6, y: 0, duration: 0),
+                    SKAction.moveBy(x: -mov_hw / 6, y: 0, duration: 3600)])))
             }
             else if i < 18 {
-                nodeH[i].position.x = 250
-                nodeH[i].position.y = 330 - 180 / 6 * CGFloat(i - 12)
+                nodeH[i].position.x = pos_hx + mov_hw
+                nodeH[i].position.y = pos_hy + mov_hh - mov_hh / 6 * CGFloat(i - 12)
                 nodeH[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeH[i].text!)
                         let tmp2 = (tmp1! + 1) % 24
                         self.nodeH[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: 0, y: -180 / 6, duration: 0),
-                    SKAction.moveBy(x: 0, y:  180 / 6, duration: 3600)])))
+                    SKAction.moveBy(x: 0, y: -mov_hh / 6, duration: 0),
+                    SKAction.moveBy(x: 0, y:  mov_hh / 6, duration: 3600)])))
             }
             else {
-                nodeH[i].position.x = 250 - 180 / 6 * CGFloat(i - 18)
-                nodeH[i].position.y = 150
+                nodeH[i].position.x = pos_hx + mov_hw - mov_hw / 6 * CGFloat(i - 18)
+                nodeH[i].position.y = pos_hy
                 nodeH[i].run(SKAction.repeatForever(SKAction.sequence([
                     SKAction.run {
                         let tmp1 = Int(self.nodeH[i].text!)
                         let tmp2 = (tmp1! + 1) % 24
                         self.nodeH[i].text = "\(tmp2)"
                     },
-                    SKAction.moveBy(x: -180 / 6, y: 0, duration: 0),
-                    SKAction.moveBy(x:  180 / 6, y: 0, duration: 3600)])))
+                    SKAction.moveBy(x: -mov_hw / 6, y: 0, duration: 0),
+                    SKAction.moveBy(x:  mov_hw / 6, y: 0, duration: 3600)])))
             }
             scene.addChild(nodeH[i])
         }
